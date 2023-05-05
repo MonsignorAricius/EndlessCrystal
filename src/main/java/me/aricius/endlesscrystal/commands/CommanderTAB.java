914dc84c1afef56ff9ace4cd8a10baead8151d52
MonsignorAricius <aricius@centrum.sk>
2023-05-05 22:06:48 +0200

    Tab completer fix, adding semi completed words to complete and no completion on the endsdf

--- src/main/java/me/aricius/endlesscrystal/commands/CommanderTAB.java
+++ src/main/java/me/aricius/endlesscrystal/commands/CommanderTAB.java
@@ -1,6 +1,5 @@
 package me.aricius.endlesscrystal.commands;
 
-import me.aricius.endlesscrystal.EndlessCrystal;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -10,10 +9,6 @@ import org.jetbrains.annotations.Nullable;
 import java.util.*;
 
 public class CommanderTAB implements TabCompleter {
-    private final EndlessCrystal plugin;
-    public CommanderTAB(EndlessCrystal plugin) {
-        this.plugin = plugin;
-    }
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
